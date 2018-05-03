5208e4319b19b60ac6de11908f9d67e2229487d4
Camilla Voss <camilla@5oss.dk>
2018-05-03 14:46:14 +0200

    Beginning structure of landing page

---
+++ landingpage/index.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Frontfill | Dynamic content app</title>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700,700i" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <nav class="navigation">
+            <ul>
+                <li><a href="#">Frontfill</a></li>
+             	<li><a href="#">How it works</a></li>
+             	<li><a href="#">Pricing</a></li>
+             	<li><a href="#" class="CTA-btn">Try it now!</a></li>
+            </ul>
+        </nav>
+
+        <div class="hero">
+        	<div class="hero-left">
+        		<h1>It is time to make your <b>static</b> site <i>dynamic</i>!</h1>
+        		<h3>Spare the hassle of content management systems, and blah blah</h3>
+        		<a href="#" class="CTA-btn">Try it now!</a>
+        	</div>
+        	<div class="hero-right">
+        		<div class="hero-right-image"></div>
+        	</div>
+        </div>
+
+        <div class="info">
+        	<h2>How It Works</h2>
+        	<div class="info-row">
+        		<div class="info-row-left">
+        			<div class="info-row-image"></div>
+        		</div>
+        		<div class="info-row-right">
+        			<h3>Create your content</h3>
+        			<p>Create the neccesary sections and elements needed on your current website. These can easily be sorted for your comfort.</p>
+        		</div>
+        	</div>
+        	<div class="info-row">
+        		<div class="info-row-left">
+        			<h3>Your developer takes over</h3>
+        			<p>Your developer will use our API to implement all the dynamic fields needed on your website. This only needs to be done a single time in order to give you control of your site!</p>
+        		</div>
+        		<div class="info-row-right">
+        			<div class="info-row-image"></div>
+        		</div>
+        	</div>
+        	<div class="info-row">
+        		<div class="info-row-left">
+        			<div class="info-row-image"></div>
+        		</div>
+        		<div class="info-row-right">
+        			<h3>Update your website - forever!</h3>
+        			<p>You are now able to update the content on you website, for as long as you will, and as many times as you want. Enjoy the freedom!</p>
+        		</div>
+        	</div>
+        </div>
+
+        <div class="pricing">
+        	<h2>Pricing</h2>
+        	<div class="pricing-row">
+        		<div class="pricing-row-left">
+        			<h2>Frontfill</h2>
+        			<ul>
+        			    <li>Unlimited API calls</li>
+        			    <li>Unlimited content elements</li>
+        			    <li>Userfriendly interface</li>
+        			</ul>
+					<a href="#" class="CTA-btn">Try it now!</a>
+					<p>at $ xx/month</p>
+        		</div>
+        		<div class="pricing-row-divider"></div>
+        		<div class="pricing-row-right">
+        			<h2>Frontfill Pro</h2>
+        			<ul>
+        				<li>Unlimited number of websites</li>
+        			    <li>Share dashboard with other users</li>
+        			    <li>Unlimited API calls</li>
+        			    <li>Unlimited content elements</li>
+        			    <li>Userfriendly interface</li>
+        			</ul>
+        			<a href="#" class="CTA-btn">Try it now!</a>
+					<p>at $ xx/month</p>
+        		</div>
+        	</div>
+        </div>
+
+        <footer class="footer">
+        	<h2>Frontfill</h2>
+        </footer>
+    </body>
+</html>
