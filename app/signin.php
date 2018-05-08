16ccd7cac38e86a87f91bbe8d2e3e4fe23f928bb
Camilla Voss <camilla@5oss.dk>
2018-05-08 12:13:46 +0200

    Sign in and sign up design

--- app/signin.php
+++ app/signin.php
@@ -4,11 +4,31 @@
         <meta charset="utf-8">
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Frontfill | Dynamic content app</title>
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700,700i" rel="stylesheet">
+        <title>Frontfill | Sign in</title>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        
+        <div class="full-size">
+            <div class="content">
+                <div class="content-image"></div>
+                <div class="form">
+                    <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+                        <h1>Sign in to your account</h1>
+
+                        <div class="form-group">
+                            <input type="email" name="email" class="form-input" placeholder="Email" required>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="password" name="password" class="form-input" placeholder="Password" required>
+                        </div>
+
+                        <button type="submit" name="signin" value="signin" class="CTA-btn">Sign in</button>
+                        <p>New user? <a href="signup.php">Sign up now!</a></p>
+                    </form>                 
+                </div>
+            </div>
+        </div> 
     </body>
 </html>
--- app/signup.php
+++ app/signup.php
@@ -4,13 +4,30 @@
         <meta charset="utf-8">
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Frontfill | Dynamic content app</title>
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700,700i" rel="stylesheet">
+        <title>Frontfill | Sign up</title>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
     	<div class="full-size">
-			<div class="form">
+			<div class="content">
+				<div class="content-image"></div>
+				<div class="form">
+					<form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+						<h1>Create new user</h1>
+
+						<div class="form-group">
+		                	<input type="email" name="email" class="form-input" placeholder="Email" required>
+		                </div>
+
+		                <div class="form-group">
+		                	<input type="password" name="password" class="form-input" placeholder="Password" required>
+		                </div>
+
+						<button type="submit" name="signup" value="signup" class="CTA-btn">Sign up</button>
+						<p>Already a user? <a href="signin.php">Log in now!</a></p>
+					</form>					
+				</div>
 			</div>
     	</div> 
     </body>
