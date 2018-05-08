de4f223a5ae577a744c54705e7e549a70b2aaa72
Camilla Voss <camilla@5oss.dk>
2018-05-08 10:46:12 +0200

    Init app

---
+++ app/signup.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Frontfill | Dynamic content app</title>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700,700i" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    	<div class="full-size">
+			<div class="form">
+			</div>
+    	</div> 
+    </body>
+</html>
